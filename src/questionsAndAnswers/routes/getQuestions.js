46c92f729d8a4af040b05403c80511d1c1bf596a
ahmedbelal15 <ahmbelal95@gmail.com>
2021-01-10 06:28:32 +0200

    added trycatch block for get questions route

--- src/questionsAndAnswers/routes/getQuestions.js
+++ src/questionsAndAnswers/routes/getQuestions.js
@@ -4,8 +4,13 @@ const client = require('../../db')
 
 router.get('/',protect,async (req,res)=>{
     const userId = req.user
-    const data = (await client.query(`SELECT question_id, sender_id, question, is_anonymous, asked_date FROM questions WHERE reciever_id = $1 ORDER BY asked_date DESC`, [userId])).rows
-    res.json(data)
+
+    try {
+        const data = (await client.query(`SELECT question_id, sender_id, question, is_anonymous, asked_date FROM questions WHERE reciever_id = $1 ORDER BY asked_date DESC`, [userId])).rows
+        res.status(200).json(data)
+    } catch (error) {
+        res.status(400).json('error')
+    }
 })
 
 module.exports = router
