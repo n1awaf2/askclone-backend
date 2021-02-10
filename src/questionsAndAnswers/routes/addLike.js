4927f79e29727e9d2b24d2dbdbb15ff93b929069
ahmedbelal15 <ahmbelal95@gmail.com>
2021-02-10 04:47:43 +0200

    added websockets for notifications

--- src/questionsAndAnswers/routes/addLike.js
+++ src/questionsAndAnswers/routes/addLike.js
@@ -3,12 +3,20 @@ const protect = require('../../auth/middlewares/protect')
 const client = require('../../db')
 const tokenSender = require("../helpers/tokenSender");
 
+
+
 router.put('/:id', protect, async(req,res)=>{
     const questionId = req.params.id
     const userId = req.user
 
     try {
         await client.query('UPDATE questions SET liked_by = array_append(liked_by, $1) WHERE question_id = $2', [userId, questionId])
+        const recieverId = (await client.query('SELECT reciever_id FROM questions WHERE question_id = $1', [questionId])).rows[0].reciever_id
+
+        global.connectedUsers[recieverId].forEach(id =>{
+          global.io.to(id).emit('like', 'U got a new like')
+        })
+        
         if (res.get("isrefreshed") === "true") {
             tokenSender(res);
           } else {
@@ -17,8 +25,8 @@ router.put('/:id', protect, async(req,res)=>{
             });
           }
     } catch (error) {
-        res.status(400).json({message: 'Error'})
-        console.log(error);
+      console.log(error);
+        return res.status(400).json({message: 'Error'})
     }
 })
 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const server = require("http").createServer(app);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
-app.use('/images', express.static('./images'))
+app.use("/images", express.static("./images"));
 //AUTH ROUTES
 const registerRoute = require("./auth/routes/register");
 const loginRoute = require("./auth/routes/login");
@@ -22,56 +28,86 @@ app.use("/auth/verifyemail", verifyEmail);
 //END OD AUTH ROUTES
 
 //QUESTION AND ANSWERS ROUTES
-const askYourselfRoute = require('./questionsAndAnswers/routes/askYourself')
-const askAnotherUserRoute = require('./questionsAndAnswers/routes/askAnotherUser')
-const getQuestionsRoute = require('./questionsAndAnswers/routes/getQuestions')
-const getOneQuestionRoute = require('./questionsAndAnswers/routes/getOneQuestion')
-const deleteQuestionRoute = require('./questionsAndAnswers/routes/deleteQuestion')
-const addAnswerRoute = require('./questionsAndAnswers/routes/addAnswer')
-const getAnswersRoute = require('./questionsAndAnswers/routes/getAnswers')
-const getOneAnswerRoute = require('./questionsAndAnswers/routes/getOneAnswer')
-const getYourAnsweresRoute = require('./questionsAndAnswers/routes/getYourAnswers')
-const addLikeRoute = require('./questionsAndAnswers/routes/addLike')
-const removeLikeRoute = require('./questionsAndAnswers/routes/removeLike')
-const removeAnswerRoute = require('./questionsAndAnswers/routes/removeAnswer')
+const askYourselfRoute = require("./questionsAndAnswers/routes/askYourself");
+const askAnotherUserRoute = require("./questionsAndAnswers/routes/askAnotherUser");
+const getQuestionsRoute = require("./questionsAndAnswers/routes/getQuestions");
+const getOneQuestionRoute = require("./questionsAndAnswers/routes/getOneQuestion");
+const deleteQuestionRoute = require("./questionsAndAnswers/routes/deleteQuestion");
+const addAnswerRoute = require("./questionsAndAnswers/routes/addAnswer");
+const getAnswersRoute = require("./questionsAndAnswers/routes/getAnswers");
+const getOneAnswerRoute = require("./questionsAndAnswers/routes/getOneAnswer");
+const getYourAnsweresRoute = require("./questionsAndAnswers/routes/getYourAnswers");
+const addLikeRoute = require("./questionsAndAnswers/routes/addLike");
+const removeLikeRoute = require("./questionsAndAnswers/routes/removeLike");
+const removeAnswerRoute = require("./questionsAndAnswers/routes/removeAnswer");
 ///////////////////////////////////////////////////////////////////////////////
-app.use('/question/askyourself', askYourselfRoute)
-app.use('/questions/getquestions', getQuestionsRoute)
-app.use('/questions/getquestion', getOneQuestionRoute)
-app.use('/questions/deletequestion', deleteQuestionRoute)
-app.use('/questions/addanswer', addAnswerRoute)
-app.use('/questions/getanswers', getAnswersRoute)
-app.use('/questions/getoneanswer', getOneAnswerRoute)
-app.use('/questions/getyouranswers', getYourAnsweresRoute)
-app.use('/questions/askquestion', askAnotherUserRoute)
-app.use('/questions/addlike', addLikeRoute)
-app.use('/questions/removelike', removeLikeRoute)
-app.use('/questions/removeAnswer', removeAnswerRoute)
+app.use("/question/askyourself", askYourselfRoute);
+app.use("/questions/getquestions", getQuestionsRoute);
+app.use("/questions/getquestion", getOneQuestionRoute);
+app.use("/questions/deletequestion", deleteQuestionRoute);
+app.use("/questions/addanswer", addAnswerRoute);
+app.use("/questions/getanswers", getAnswersRoute);
+app.use("/questions/getoneanswer", getOneAnswerRoute);
+app.use("/questions/getyouranswers", getYourAnsweresRoute);
+app.use("/questions/askquestion", askAnotherUserRoute);
+app.use("/questions/addlike", addLikeRoute);
+app.use("/questions/removelike", removeLikeRoute);
+app.use("/questions/removeAnswer", removeAnswerRoute);
 ///////////////////////////////////////////////////////////////////////////////
-const imageUploadRoute = require('./misc/uploadimage')
-const updateSettingsRoute = require('./misc/updateSettings')
-const getUserSettingsRoute = require('./misc/getUserSettings')
-const getUserNameAndImageRoute = require('./misc/getUserNameAndImage')
+const imageUploadRoute = require("./misc/uploadimage");
+const updateSettingsRoute = require("./misc/updateSettings");
+const getUserSettingsRoute = require("./misc/getUserSettings");
+const getUserNameAndImageRoute = require("./misc/getUserNameAndImage");
 
-app.use('/upload/image', imageUploadRoute)
-app.use('/user/settings', updateSettingsRoute)
-app.use('/user/getsettings', getUserSettingsRoute)
-app.use('/user/getuserandimage', getUserNameAndImageRoute)
-
-///////////////////////////////////////////////////////////////////////////////
-const followRoute = require('./following/routes/follow')
-const unfollowRoute = require('./following/routes/unfollow')
-const checkFollowStatusRoute = require('./following/routes/checkFollowingStatus')
-const getFollowingRoute = require('./following/routes/getFollowing')
-
-app.use('/follow', followRoute)
-app.use('/unfollow', unfollowRoute)
-app.use('/checkfollow', checkFollowStatusRoute)
-app.use('/getfollowing', getFollowingRoute)
+app.use("/upload/image", imageUploadRoute);
+app.use("/user/settings", updateSettingsRoute);
+app.use("/user/getsettings", getUserSettingsRoute);
+app.use("/user/getuserandimage", getUserNameAndImageRoute);
 
 ///////////////////////////////////////////////////////////////////////////////
+const followRoute = require("./following/routes/follow");
+const unfollowRoute = require("./following/routes/unfollow");
+const checkFollowStatusRoute = require("./following/routes/checkFollowingStatus");
+const getFollowingRoute = require("./following/routes/getFollowing");
+
+app.use("/follow", followRoute);
+app.use("/unfollow", unfollowRoute);
+app.use("/checkfollow", checkFollowStatusRoute);
+app.use("/getfollowing", getFollowingRoute);
+
+///////////////////////////////////////////////////////////////////////////////
+//SocketIO logic
+global.io = io;
+const connectedUsers = {}
+global.connectedUsers = connectedUsers
+const socketsAndUsers = {}
+
+io.on("connection", (socket) => {
+  socket.on("connected", (userId) => {
+    if (!connectedUsers[userId]) {
+      connectedUsers[userId] = [socket.id];
+    } else {
+      connectedUsers[userId].push(socket.id);
+    }
+    socketsAndUsers[socket.id] = userId
+  });
 
 
-app.listen(4000, () => {
-  console.log('listening on :4000');
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+    const disconnectUser = socketsAndUsers[socket.id]
+    delete socketsAndUsers[socket.id]
+    if(connectedUsers[disconnectUser]){
+      const newUserArray = connectedUsers[disconnectUser].filter(id => id !== socket.id)
+      connectedUsers[disconnectUser] = newUserArray
+    }
+
+  });
+
+
+});
+
+///////////////////////////////////////////////////////////////////////////////
+server.listen(4000, () => {
+  console.log("listening on :4000");
 });
