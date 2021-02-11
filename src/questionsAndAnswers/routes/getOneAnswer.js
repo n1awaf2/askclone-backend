0b6b1bb953d3398e6d293235ffe5f72c87a7a3b8
ahmedbelal15 <ahmbelal95@gmail.com>
2021-02-11 21:10:44 +0200

    modified getOneAnswer queries to get author's data

--- src/questionsAndAnswers/routes/getOneAnswer.js
+++ src/questionsAndAnswers/routes/getOneAnswer.js
@@ -8,14 +8,22 @@ router.get("/:answerid", protect, async (req, res) => {
   try {
     const data = (
       await client.query(
-        "SELECT question_id, sender_id, reciever_id, answer, question, is_anonymous, asked_date, answered_date, liked_by, answer_image, user_name, user_image  FROM questions INNER JOIN users_data ON questions.sender_id = users_data.user_id WHERE questions.question_id = $1 AND answer IS NOT NULL",
+        `SELECT question_id, sender_id, reciever_id, answer, question, is_anonymous, asked_date, answered_date, liked_by, answer_image, user_name AS questioner_name, user_image AS questioner_image
+          FROM questions INNER JOIN users_data ON questions.sender_id = users_data.user_id 
+          WHERE questions.question_id = $1 AND answer IS NOT NULL`,
         [answerId]
       )
     ).rows[0];
-    console.log(data);
+
     if (data === undefined) {
       throw new Error("this question has no answer");
     }
+
+    const authorId = data.reciever_id
+    const authorData = (await client.query('SELECT user_image AS author_image, user_name AS author_name FROM users_data WHERE user_id = $1', [authorId])).rows[0]
+    data.author_image = authorData.author_image
+    data.author_name = authorData.author_name
+
     if (res.get("isrefreshed") === "true") {
       tokenSender(res, data);
     } else {
